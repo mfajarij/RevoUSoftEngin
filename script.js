7b28a59b3c3985b34d03d227e74176fc92653f2f
mfajarij <56795220+mfajarij@users.noreply.github.com>
2023-07-20 00:50:09 +0700

    First commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// function HelloWorld () {
-//     alert("Hello Warudo")
-// }
+// // function HelloWorld () {
+// //     alert("Hello Warudo")
+// // }
 
-// var num1 = 5;
+// // var num1 = 5;
 
-// var num2 = 10;
+// // var num2 = 10;
 
-// var result = num1 > num2;
+// // var result = num1 > num2;
+
+// function peringatan(){
+//     // console.log("ini sudah diklik");
+//     let pertama = document.getElementById("angka_pertama").value;
+//     let kedua= document.getElementById("angka_kedua").value;
+    
+//     let hasil = parseInt(pertama) + parseInt(kedua);
+
+//     console.log(hasil)
